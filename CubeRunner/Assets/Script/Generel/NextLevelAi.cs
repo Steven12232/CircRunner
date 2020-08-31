e10936fe256f51fce6daf0c7f8a219f3fb713c76
JonathanFarkas <jjasl777222@gmail.com>
2020-08-31 09:36:18 -0400

    Revert "Messup"
    This reverts commit 5f4d5221fcfdcc0180f18d26ab50f9022979128b.

--- CubeRunner/Assets/Script/Generel/NextLevelAi.cs
+++ CubeRunner/Assets/Script/Generel/NextLevelAi.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Diagnostics;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -18,7 +17,7 @@ public class NextLevelAi : MonoBehaviour
     private Rigidbody2D PlayerRB;
     private SpriteRenderer spriteRenderer;
 
-    public static float IncreasedTime = 1;
+    private static float IncreasedTime = 1;
     
     private GameObject[] NumberOfAiLeft;
     
@@ -50,18 +49,9 @@ public class NextLevelAi : MonoBehaviour
     }
 
     
-    public float TimeIncreaseValue()
+    float TimeIncreaseValue()
     {
-
-        if (Time.timeScale == 5.0f)
-        {
-            IncreasedTime = 5;
-        }
-        else if (Time.timeScale != 5.0f)
-        {
-            IncreasedTime = IncreasedTime + 0.05f;
-        }
-        
+        IncreasedTime = IncreasedTime + 0.05f;
 
         return IncreasedTime;
     }
@@ -72,9 +62,9 @@ public class NextLevelAi : MonoBehaviour
 
         TimeIncreaseValue();
 
+        Time.timeScale = TimeIncreaseValue();
         
         LevelNumber++;
-        
         
         AddBounceToPlayer();
         
@@ -130,10 +120,6 @@ public class NextLevelAi : MonoBehaviour
      { 
          NumberOfAiLeft = GameObject.FindGameObjectsWithTag("AI");
          CheckForNoAiLeft();
-         Time.timeScale = IncreasedTime;
-
-         Debug.Log(IncreasedTime);
-
          LevelText.text = "Level:" + LevelNumber;
     }
 }
